7ea5b359930dae6b4411a0f838e77b32b9aa3b9b
Genious-Hub <44255872+timegamerkill2@users.noreply.github.com>
2021-12-16 11:36:00 +0700

    Update and rename README.md to Loader.lua

---
+++ Loader.lua
@@ -0,0 +1,4 @@
+if game.GameId == 3351674303 then
+    loadstring(game:HttpGet(("https://raw.githubusercontent.com/timegamerkill2/Driving-Empire/main/Loader.lua"), true))()
+    return
+end
--- README.md
+++
@@ -1,1 +0,0 @@
-# Genious-Hub
