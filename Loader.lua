99d3110455747ef83abc232794d3cafdb72eebdf
Genious-Hub <44255872+timegamerkill2@users.noreply.github.com>
2021-12-19 21:28:20 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,6 +1,7 @@
-if game.GameId == 3351674303 then
-    loadstring(game:HttpGet(("https://raw.githubusercontent.com/timegamerkill2/Driving-Empire/main/Loader.lua"), true))()
-else
-if game.GameId == 7429434108 then
-    loadstring(game:HttpGet(("https://raw.githubusercontent.com/timegamerkill2/Driving-Empire/main/Loader.lua"), true))()
+local place = game.PlaceId
+
+spawn(function()
+if place == 335167430 then --https://www.roblox.com/games/3351674303/Driving-Empire
+print("Loaded GUI")
+loadstring(game:HttpGet("https://raw.githubusercontent.com/timegamerkill2/Driving-Empire/main/Loader.lua", true))()
 end
